c8f54b2276c6fb6cd456ba78a18a8300c732eb67
matovu-farid <matovu90@gmail.com>
2024-06-25 14:02:45 +0300

    Add destroy dependent on block apartment and resident

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,12 +1,19 @@
 class Apartment < ApplicationRecord
   include Adminable
-  validates :price, presence: true 
+  validates :price, presence: true
   validates :floor, presence: true
   validates :name, presence: true
   belongs_to :block
   has_many :users, through: :block
   alias_attribute :admins, :users
-  scope :filter_by_admin, ->(user) { Apartment.joins(block: {block_admins: :user}).includes(block: {block_admins: :user}).where({users:{id: user.id}})}
-  has_one :resident
+  scope(
+    :filter_by_admin,
+    -> (user) {
+      Apartment.joins(block: {block_admins: :user}).includes(block: {block_admins: :user}).where(
+        {users: {id: user.id}}
+      )
+    }
+  )
+  has_one :resident, dependent: :destroy
 
 end
--- app/models/block.rb
+++ app/models/block.rb
@@ -1,8 +1,8 @@
 class Block < ApplicationRecord
-  has_many :apartments
-  has_many :block_admins
+  has_many :apartments, dependent: :destroy
+  has_many :block_admins, dependent: :destroy
   has_many :users, through: :block_admins
   alias_attribute :admins, :users
-  
-  scope :filter_by_admin, ->(user) { Block.includes(:block_admins).where(block_admins:{user: user})}
+
+  scope :filter_by_admin, -> (user) { Block.includes(:block_admins).where(block_admins: {user: user}) }
 end
