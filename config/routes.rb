d2e7c5f7644246c02fe956161871ac8772aa1915
matovu-farid <matovu90@gmail.com>
2024-06-29 05:18:33 +0300

    Change defaault homepage when signed in

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   devise_for(:users)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   authenticated(:user) do
-    root(to: "home#index", as: :authenticated_root)
+    root(to: "apartments#index", as: :authenticated_root)
   end
 
   root(to: redirect("/users/sign_in"))
