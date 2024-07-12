caafac72145d2836603adb1108d6b3e9f10e5b36
matovu-farid <matovu90@gmail.com>
2024-07-12 08:34:26 +0300

    Removed switch import

--- app/javascript/entrypoints/application.tsx
+++ app/javascript/entrypoints/application.tsx
@@ -4,12 +4,10 @@ import 'react-dom'
 import { createRoot } from 'react-dom/client';
 import MyApp from '../components/App';
 
-import { Switch } from "@components/ui/switch"
 
 function SwitchDemo() {
   return (
     <div className="flex items-center space-x-2 bg-black">
-      <Switch id="airplane-mode" className="w-[50px] h-[20px]" />
     </div>
   )
 }
