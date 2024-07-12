b5e4feaf2ec72dfadfd42fd61f0def54695043fb
matovu-farid <matovu90@gmail.com>
2024-07-12 06:35:02 +0300

    When a new resideent is created, they fill an apartment

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -36,15 +36,21 @@ class ResidentsController < ApplicationController
     apartment.isOccupied = true
     rent_session = RentSession.new(paymentDueDate: @resident.startdate, resident: @resident, apartment: apartment)
 
+    begin
+      ActiveRecord::Base.transaction do
+        @resident.save!
+        rent_session.save!
+        apartment.save!
+      end
 
-    respond_to do |format|
-      if @resident.save && rent_session.save
+      respond_to do |format|
         format.html { redirect_to(resident_url(@resident), notice: "Resident was successfully created.") }
         format.json { render(:show, status: :created, location: @resident) }
-      else
-        format.html { render(:new, status: :unprocessable_entity) }
-        format.json { render(json: @resident.errors, status: :unprocessable_entity) }
       end
+
+    rescue ActiveRecord::RecordInvalid => e
+      format.html { render(:new, status: :unprocessable_entity) }
+      format.json { render(json: @resident.errors, status: :unprocessable_entity) }
     end
   end
 
