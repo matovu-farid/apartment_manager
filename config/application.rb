373e9fa64f57a826d9775a3002e69cf61d007382
matovu-farid <matovu90@gmail.com>
2024-07-16 08:08:31 +0300

    Only add pdf generation for receipt page

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,8 @@ module ApartmentManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.middleware.use(WickedPdf::Middleware)
+    config.middleware.use(WickedPdf::Middleware, {}, only: %r{.*/receipt.*})
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
