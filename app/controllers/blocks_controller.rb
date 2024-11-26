4e27bbea8b86d6007635c8b5ce8c376adc750970
matovu-farid <matovu90@gmail.com>
2024-11-26 15:53:02 +0300

    Try authenticating block before showing block

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class BlocksController < ApplicationController
+  before_action :authenticate_user!
   load_and_authorize_resource
 
   before_action :set_block, only: %i[show edit update destroy]
 
-  before_action :authenticate_user!
   # GET /blocks or /blocks.json
   def index
     @blocks = Block.accessible_by(current_ability)
